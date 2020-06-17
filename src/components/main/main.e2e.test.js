a51e9f58b46baecd3ec3b5ae9e36df1a15daa80c
Yarisson18 <gemini8811@gmail.com>
2020-06-17 20:17:52 +0300

    правки в тест, добавление описания props

--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -11,8 +11,20 @@ describe(`Main component`, () => {
   it(`Should locations item be pressed`, () => {
     const onLocationsItemClick = jest.fn();
 
+    const hotels = [
+      {img: `img/apartment-01.jpg`, price: `&euro;200`, width: `100%`, title: `Beautiful &amp; luxurious apartment at great location`, type: `Apartment`},
+      {img: `img/room.jpg`, price: `&euro;140`, width: `20%`, title: `Wood and stone place`, type: `Private room`},
+      {img: `img/apartment-02.jpg`, price: `&euro;52`, width: `60%`, title: `Canal View Prinsengracht`, type: `Apartment`},
+      {img: `img/apartment-03.jpg`, price: `&euro;85`, width: `80%`, title: `Nice, cozy, warm big bed apartment`, type: `Apartment`},
+      {img: `img/room.jpg`, price: `&euro;150`, width: `40%`, title: `Wood and stone place`, type: `Private room`},
+    ];
+
+    const places = 120;
+
     const main = shallow(
         <Main
+          hotels={hotels}
+          places={places}
           onLocationsItemClick={onLocationsItemClick}
         />
     );
